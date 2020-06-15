72de3e2bd6c0338867c69462e1f811ff00b2ec47
artem <mstrJesm@gmail.com>
2020-06-15 23:57:03 +0300

    refactoring reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,21 +2,21 @@ const bookLoaded = (newBooks) => {
   return {
     type: 'FETCH_BOOKS_SUCCESS',
     payload: newBooks
-  }
-}
+  };
+};
 
 const booksRequested = () => {
   return {
     type: 'FETCH_BOOKS_REQUEST',
-  }
-}
+  };
+};
 
 const booksError = (error) => {
   return {
     type: 'FETCH_BOOKS_FAILURE',
     payload: error
-  }
-}
+  };
+};
 
 
 const fetchBooks = (bookstoreService, dispatch) => () => {
@@ -24,9 +24,33 @@ const fetchBooks = (bookstoreService, dispatch) => () => {
   bookstoreService.getBooks()
     .then((data) => dispatch(bookLoaded(data)))
     .catch(err => dispatch(booksError(err)));
+};
+
+const bookAddedToCart = (bookId) => {
+  return {
+    type: 'BOOK_ADDED_TO_CART',
+    payload: bookId
+  };
+};
+
+const bookRemovedFomCart = (bookId) => {
+  return {
+    type: 'BOOK_REMOVED_FROM_CART',
+    payload: bookId
+  }
+}
+
+const allBooksRemovedFomCart = (bookId) => {
+  return {
+    type: 'ALL_BOOKS_REMOVED_FROM_CART',
+    payload: bookId
+  }
 }
 
 export {
-  fetchBooks
+  fetchBooks,
+  bookAddedToCart,
+  bookRemovedFomCart,
+  allBooksRemovedFomCart
 };
 
